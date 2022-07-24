c7fe8ec6f3ebf0047213f6962e5ccbe9f8dab691
ToniIvars <toniivaju@gmail.com>
2022-07-25 00:35:33 +0200

    Add github icon without Font Awesome and add nav link selection when the page is scrolled

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const navLinks = document.querySelectorAll('.nav-link')
+const NAV_LINKS = [...document.querySelectorAll('.nav-link')]
+const SECTIONS_POSITIONS = [...document.querySelectorAll('.section')]
 
-navLinks.forEach(link => link.onclick = () => selectLink(link))
+var navLinkClicked = false
 
-const selectLink = link => {
-  navLinks.forEach(navLink => {
-    navLink.classList.toggle('selected', link === navLink)
+window.onscroll = () => {
+  if (navLinkClicked) return
+
+  let current = ""
+
+  SECTIONS_POSITIONS.forEach(section => {
+    const sectionTop = section.offsetTop
+    if (scrollY >= sectionTop ) {
+      current = section.getAttribute("id")
+    }
   })
+
+  NAV_LINKS.forEach(li => li.classList.toggle('selected', li.dataset.section === current))
 }
+
+NAV_LINKS.forEach(li => {
+  li.onclick = ({target}) => {
+    navLinkClicked = true
+    
+    NAV_LINKS.forEach(li => li.classList.toggle('selected', li === target))
+
+    setTimeout(() => {
+      navLinkClicked = false
+    }, 1000)
+  }
+})
